8715e52a53a9cbd218d2b0758a512051c4a3f0b0
Provd5 <provd12345@gmail.com>
2023-06-28 16:43:34 +0200

    added eslint

--- src/env.mjs
+++ src/env.mjs
@@ -5,20 +5,12 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
   },
-
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
-
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+  experimental__runtimeEnv: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
-
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
-   */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
