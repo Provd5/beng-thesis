5bb9d2da937e166f71b36502a6e4b60f322ce6ec
Provd5 <provd12345@gmail.com>
2023-10-16 21:48:33 +0200

    search page fix

--- app/[locale]/(subpages)/search/page.tsx
+++ app/[locale]/(subpages)/search/page.tsx
@@ -1,28 +1,7 @@
-import { createTranslator } from "next-intl";
-
-import { Button, ButtonLink, ButtonWhite } from "~/components/ui/Buttons";
-
-import { getMessages, type PageProps } from "../../layout";
-
-export default async function SearchPage({ params: { locale } }: PageProps) {
-  const messages = await getMessages(locale);
-  const t = createTranslator({ locale, messages });
+export default function SearchPage() {
   return (
     <>
-      <div className="flex flex-col items-center gap-3">
-        <h1>{t("categoryTitles")}</h1>
-        <ButtonWhite>TEST test Test</ButtonWhite>
-        <ButtonWhite size="sm">TEST test Test</ButtonWhite>
-        <ButtonWhite loading>TEST test Test</ButtonWhite>
-        <Button>TEST test Test</Button>
-        <Button size="sm">TEST test Test</Button>
-        <Button loading>TEST test Test</Button>
-        <ButtonLink>TEST test Test</ButtonLink>
-        <ButtonLink size="sm" active>
-          TEST test Test
-        </ButtonLink>
-        <ButtonLink loading>TEST test Test</ButtonLink>
-      </div>
+      <div>SearchPage</div>
     </>
   );
 }
