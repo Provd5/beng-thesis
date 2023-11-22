6a0269ed779347e219be07300cabacb4b27f21db
Provd5 <102181040+Provd5@users.noreply.github.com>
2023-11-22 16:56:07 +0100

    added captcha and site env (#33)

--- env.mjs
+++ env.mjs
@@ -7,11 +7,15 @@ export const env = createEnv({
     DIRECT_URL: z.string().url(),
   },
   client: {
+    SITE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_HCAPTCHA_SITEKEY: z.string(),
   },
   experimental__runtimeEnv: {
+    SITE_URL: process.env.SITE_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_HCAPTCHA_SITEKEY: process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY,
   },
 });
